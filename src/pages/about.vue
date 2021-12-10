7310e1f8bf893f5111730e545b987790a801e522
shenjin <shenjin@dxy.cn>
2021-12-10 18:42:31 +0800

    初始化

---
+++ src/pages/about.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1 class="aaa">这是about页面</h1>
+</template>
---
+++ src/pages/home.vue
@@ -0,0 +1,12 @@
+<template>
+<div>
+    <h1 class="text-3xl font-bold underline">这是关于页面accb</h1>
+    <div>
+        <todo-list></todo-list>
+    </div>
+</div>
+</template>
+
+<script setup>
+        import TodoList from './../components/todo-list.vue'
+</script>
